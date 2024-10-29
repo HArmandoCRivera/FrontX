90bc7b1de0ee47104087db1b45278fcf6c79c3f0
Juan Esteban Tobón Duque <juanestd1013@hotmail.com>
2024-10-29 18:38:07 -0500

    .

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,5 +1,4 @@
 import XSvg from "../svgs/X";
-
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
@@ -14,46 +13,33 @@ const Sidebar = () => {
 	};
 
 	return (
-		<div className='md:flex-[2_2_0] w-18 max-w-52'>
-			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
+		<div className='w-20 md:w-52 flex-shrink-0'>
+			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700'>
 				<Link to='/' className='flex justify-center md:justify-start'>
 					<XSvg className='px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900' />
 				</Link>
 				<ul className='flex flex-col gap-3 mt-4'>
 					<li className='flex justify-center md:justify-start'>
-						<Link
-							to='/'
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
+						<Link to='/' className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4'>
 							<MdHomeFilled className='w-8 h-8' />
 							<span className='text-lg hidden md:block'>Home</span>
 						</Link>
 					</li>
 					<li className='flex justify-center md:justify-start'>
-						<Link
-							to='/notifications'
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
+						<Link to='/notifications' className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4'>
 							<IoNotifications className='w-6 h-6' />
 							<span className='text-lg hidden md:block'>Notifications</span>
 						</Link>
 					</li>
-
 					<li className='flex justify-center md:justify-start'>
-						<Link
-							to={`/profile/${data?.username}`}
-							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
-						>
+						<Link to="/profile" className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4'>
 							<FaUser className='w-6 h-6' />
 							<span className='text-lg hidden md:block'>Profile</span>
 						</Link>
 					</li>
 				</ul>
 				{data && (
-					<Link
-						to={`/profile/${data.username}`}
-						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
-					>
+					<Link to="/profile" className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'>
 						<div className='avatar hidden md:inline-flex'>
 							<div className='w-8 rounded-full'>
 								<img src={data?.profileImg || "/avatar-placeholder.png"} />
@@ -72,4 +58,5 @@ const Sidebar = () => {
 		</div>
 	);
 };
+
 export default Sidebar;
